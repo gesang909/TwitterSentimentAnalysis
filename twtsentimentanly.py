6027d27abace249ddf59222f2d522cb47092f995
gesang909 <65072564+gesang909@users.noreply.github.com>
2022-07-21 03:52:15 +0700

    Twitter sentiment analysis
    Project for completing a subject in my college

---
+++ twtsentimentanly.py
@@ -0,0 +1,145 @@
+import imp
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+from deep_translator import GoogleTranslator
+import googletrans
+import apikey
+
+  
+class TwitterClient(object):
+    
+    '''
+    Generic Twitter Class for sentiment analysis.
+    '''
+
+    def __init__(self):
+        '''
+        Class constructor or initialization method.
+        '''
+        # keys and tokens from the Twitter Dev Console put in a different file
+        
+        # attempt authentication
+        try:
+            # create OAuthHandler object
+            self.auth = OAuthHandler(apikey.consumer_key, apikey.consumer_secret)
+            # set access token and secret
+            self.auth.set_access_token(apikey.access_token, apikey.access_token_secret)
+            # create tweepy API object to fetch tweets
+            self.api = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
+            print(apikey.consumer_key)
+            print(apikey.access_token_secret)
+            print(apikey.consumer_secret)
+            print(apikey.access_token)
+  
+    def clean_tweet(self, tweet):
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
+        '''
+        tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", " ", tweet) #Remove http links
+        tweet = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet)#Remove @ sign
+        tweet = tweet.replace("#", "").replace("_", " ") #Remove hashtag sign but keep the text
+        tweet = ' '.join(tweet.split())
+        return tweet
+  
+    def get_tweet_sentiment(self, tweet):
+        '''
+        Utility function to classify sentiment of passed tweet
+        using textblob's sentiment method
+        '''
+        # create TextBlob object of passed tweet text
+        # Translate ke Inggris
+        tweet = GoogleTranslator(source="id", target="en").translate(tweet)
+        analysis = TextBlob(self.clean_tweet(tweet))
+        # print(type(analysis))
+        # set sentiment
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'        
+  
+    def get_tweets(self, query, count = 10):
+        '''
+        Main function to fetch tweets and parse them.
+        '''
+        # empty list to store parsed tweets
+        tweets = []
+  
+        try:
+            # call twitter api to fetch tweets
+            fetched_tweets = self.api.search_tweets(q = query, count = count)
+            # parsing tweets one by one
+            for tweet in fetched_tweets:
+                # empty dictionary to store required params of a tweet
+                parsed_tweet = {}
+  
+                # saving text of tweet
+                parsed_tweet['text'] = tweet.text
+                # saving sentiment of tweet
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+  
+                # appending parsed tweet to tweets list
+                if tweet.retweet_count > 0:
+                    # if tweet has retweets, ensure that it is appended only once
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+            print(type(tweets))
+            # for tw in tweets:
+            #     print(tw)
+            # return parsed tweets
+            return tweets
+  
+        except tweepy.TweepError as e:
+            # print error (if any)
+            print("Error : " + str(e))
+  
+def main():
+    # creating object of TwitterClient Class
+    api = TwitterClient()
+
+    # calling function to get tweets
+    searchtweet = input("Query = ")
+    tweets = api.get_tweets(query = searchtweet, count = 100)
+    
+    # picking positive tweets from tweets
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    print("Jumlah Positif Tweet",ptweets.count)
+    # percentage of positive tweets
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    # picking negative tweets from tweets
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    print("Jumlah Negatif Tweet",ntweets.count)
+
+    # percentage of negative tweets
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+
+    neutweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
+    print("Jumlah Netral Tweet",neutweets.count)
+
+    # percentage of neutral tweets
+    print("Neutral tweets percentage: {} %".format(100*(len(tweets)-(len( ntweets )+len( ptweets)))/len(tweets)))
+  
+    # printing first 5 positive tweets
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:10]:
+        print(tweet['text'])
+    # printing first 5 negative tweets
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:10]:
+        print(tweet['text'])
+    # printing first 5 neutral tweets
+    print("\n\nNeutral tweets:")
+    for tweet in neutweets[:10]:
+        print(tweet['text'])
+  
+if __name__ == "__main__":
+    # calling main function
+    main()
